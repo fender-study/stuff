2472ddecd9a2b7459dc2537cc103b72683b4fe37
Dima <144616994+fender-study@users.noreply.github.com>
2023-11-07 13:27:37 +0200

    Update firewall1.sh

--- Lab/firewall1.sh
+++ Lab/firewall1.sh
@@ -7,3 +7,5 @@ iptables -A INPUT -p tcp  --dport 22 -j DROP
 # Drops outgoing http and https traffic
 iptables -A OUTPUT -p tcp --dport 80 -j DROP
 iptables -A OUTPUT -p tcp --dport 443 -j DROP
+
+iptables - t nat -A POSTROUTING -s 10.0.0.0/8 -o enp0s3 -j SNAT --too-source 80.0.0.1
