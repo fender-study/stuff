19f70c44064a8c38d1cf2d5151ea8c8b05b30575
Dima <chernozdmitriy@gmail.com>
2023-11-11 14:36:09 +0200

    snake game

---
+++ python_projects/snake_game/scoreboard.py
@@ -0,0 +1,38 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("C:/Users/ihort/Desktop/data.txt", "r") as file:
+            self.high_score = int(file.read())
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_high_score()
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def write_high_score(self):
+        with open("C:/Users/ihort/Desktop/data.txt", "w") as file:
+            file.write(str(self.high_score))
